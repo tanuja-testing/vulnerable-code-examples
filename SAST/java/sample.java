327d2c276468fd995935db3ed863fb29538128c6
tanujamuthyala <tanuja@flyingduck.io>
2026-05-12 12:43:04 +0530

    Update sample.java

--- SAST/java/sample.java
+++ SAST/java/sample.java
@@ -5,6 +5,3 @@ public class Sample {
     }
   }
 
-// (CWE-259)
-// This sample Java file includes code that prints a sensitive password to the console. 
-// It can be used to test SAST tools' capability to detect hardcoded passwords.
